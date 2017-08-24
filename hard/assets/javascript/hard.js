ac1dedcc7fc962e8d694b5954a9d403154b8540b
Amanda Maifeld <a.maifeld@hotmail.com>
2017-08-24 09:19:40 -0700

    hard challenge

--- hard/assets/javascript/hard.js
+++ hard/assets/javascript/hard.js
@@ -17,9 +17,9 @@
  *    and then from red to black when clicked again.X
  *
  * 2. The links on the side of the page – "your name," "section 1," "section 2," etc. –
- *    hide every odd-numbered link in the menu.
+ *    hide every odd-numbered link in the menu.X
  *
- * 3. Change every instance of the word "bacon" on the page to be "LASER VISION"
+ * 3. Change every instance of the word "bacon" on the page to be "LASER VISION"X
  *
  * 4. Delete the last two posts in the middle section (they have a CSS class "post"
  *
@@ -32,6 +32,11 @@
  (function(){
 
    $('h1').css("color", "rgb(0,0,0)");//rgb(0,0,0)=black  rgb(255,0,0)=red
+   $('h5:odd').hide();
+$('aside p').remove();
+
+
+
 
    //jQuery equivelent to window.onload = function{}
    //code in here wont run until page loads
@@ -41,12 +46,17 @@
 
     $('h1').click(function(){
       if($('h1').css('color')=="rgb(0, 0, 0)"){
-        $('h1').css("color", "rgb(255,0,0)");
+        $('h1').css("color", "rgb(255, 0, 0)");
       } else {
-        $('h1').css("color", "rgb(0,0,0)");
+        $('h1').css("color", "rgb(0, 0, 0)");
       }
    });
 
+  //$("p").html($("p").html().replace('Bacon','LASER VISION'));
+
+   $('.post').slice(-2).remove();
+   $('.post').slice(-1).remove();
+   $('img').attr("src",'https://mi-od-live-s.legocdn.com/r/www/r/catalogs/-/media/catalogs/characters/lbm%20characters/primary/70900_1to1_batman_360_480.png?l.r2=1668006940');
 
 
 
